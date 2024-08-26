e2389660be80c1366330e90a80f7081709486247
Agnes <agnes.mccall5@gmail.com>
2024-08-25 20:37:25 -0400

    Add minDepth function

--- binary-tree.js
+++ binary-tree.js
@@ -17,7 +17,21 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
-
+    if (!this.root) return 0;
+    let minDepth;
+    let depth = 1;
+    let nodesToVisit = [this.root];
+    while (nodesToVisit.length > 0) {
+      let node = nodesToVisit.pop();
+      if (!node.left && !node.right) {
+        if (!minDepth || depth < minDepth) minDepth = depth;
+        depth = 1;
+      }
+      if (node.left) nodesToVisit.push(node.left);
+      if (node.right) nodesToVisit.push(node.right);
+      depth++;
+    }
+    return minDepth;
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
